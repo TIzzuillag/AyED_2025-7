573167453c56339d0881677e06e16bc47212fc2e
TIzzuillag <galluzzitizi@gmail.com>
2026-04-21 15:29:29 -0300

    Add files via upload

---
+++ tp practica mandalorian 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nave;
+            int pesoextra;
+            Console.WriteLine("Ingrese el peso de carga;");
+            nave = Convert.ToInt32(Console.ReadLine());
+            if (nave <= 5000)
+            {
+             Console.WriteLine("Despegue autorizado. Buen viaje, Mando");
+            }
+             else
+              {
+                pesoextra = nave - 5000;
+                Console.WriteLine("Alerta: Exceso de peso. Debes descargar" +  pesoextra, "kg para despegar");
+              }
+
+
+        }
+    }
+}
