5954bb312ef1eca3b938e91492fed2d722a44401
TIzzuillag <galluzzitizi@gmail.com>
2026-05-07 15:56:43 -0300

    Add files via upload

---
+++ 9 gastos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los gastos de los 5 dias");
+
+            int acumulador = 0;
+
+            int contador = 0;
+
+            for (int = i; i <= 5; i++) 
+            {
+                Console.Write("Cual fue el gasto del dia" + i);
+
+                int plata = Convert.ToInt32(Console.ReadLine());
+
+                acumulador = acumulador + plata;
+
+                if (plata > 500)
+                {
+                    contador++;
+                }
+                Console.WriteLine("El gasto total es de: " + acumulador);
+                Console.WriteLine("Las veces que el gasto es menos de 0 es : " + contador);
+
+               
+            }
+        }
+}
