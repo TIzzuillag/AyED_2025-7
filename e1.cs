7143a1a0a42a9cd36853df77f3a6d973f45c74ef
TIzzuillag <galluzzitizi@gmail.com>
2026-05-19 15:26:21 -0300

    Add files via upload

---
+++ e1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Comenzando cuenta regresiva:");
+
+            while (numero >= 0)
+
+            {
+                Console.WriteLine("Numero:" + numero);
+                numero--;
+            }
+        
+
+
+
+        }
+    }
+}
