65c38e3b832dc78f39a95b08222575bc0dfd99fe
TIzzuillag <galluzzitizi@gmail.com>
2026-05-21 16:04:18 -0300

    Add files via upload

---
+++ E5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la contraseña");
+            string contraseña = (Console.ReadLine());
+            string contra;
+            do
+            {
+                Console.WriteLine("Contraseña erronea");
+
+                contra = (Console.ReadLine());
+                if (contraseña != contra)
+                {
+                    Console.WriteLine("error");
+                }
+
+            } while (contraseña != contra);
+            Console.WriteLine("Contraseña correcta");
+
+        }
+    }
+}
