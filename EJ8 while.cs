841843f18df967f821d4d4e19b893a91f687f676
TIzzuillag <galluzzitizi@gmail.com>
2026-05-28 14:44:17 -0300

    Add files via upload

---
+++ EJ8 while.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese la temperatura");
+            int temperatura = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                temperatura = temperatura - 2;
+                Console.WriteLine("enfriando " + temperatura);
+
+                temperatura--;
+            } while (temperatura > 24);
+            Console.WriteLine("temperatura ideal alcanzada " + temperatura);
+
+        }
+    }
+}
