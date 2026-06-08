ba106f1c14c53f596bdb7208dffe041a12f40ecd
TIzzuillag <galluzzitizi@gmail.com>
2026-06-08 18:30:17 -0300

    Add files via upload

---
+++ Do-While 2/tp 10.3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            Console.WriteLine("desea ingresar una venta (S/N).");
+            int monto=0;
+            int recau = 0;
+            string opcion;
+            int contador = 0;
+            int mayor=0;
+            do
+            {
+
+                opcion = (Console.ReadLine());
+                if (opcion ==  "S")
+                {
+                    Console.Write("ingrese el monto de la venta ");
+
+                  
+                    monto = Convert.ToInt32(Console.ReadLine());
+                    recau = recau + monto;
+                    if (monto>mayor)
+                    {
+                       mayor=monto;
+
+                    }
+
+                   
+                    
+
+
+                        contador++;
+                   Console.WriteLine("Si desea continuar ingresando más ventas(S / N)");
+                }
+
+
+            } while (opcion!="N");
+            Console.WriteLine("hubo "+ contador+ " ventas" );
+            Console.WriteLine("la venta mayor fue de " + mayor );
+            Console.WriteLine("recaudado:" + recau);
+        }
+
+        }
+    }
+
---
+++ Do-While 2/tp10.5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string opcion;
+            int cero = 0;
+            int positivos = 0;
+            int negativos = 0;
+            do
+            {
+                Console.WriteLine("quiere ingresar un numero(y/n)");
+                opcion = (Console.ReadLine());
+                if (opcion == "y")
+                {
+                    Console.Write("ingrese un numero: ");
+                    num = Convert.ToInt32(Console.ReadLine());
+                    if (num > 0)
+                    {
+                        positivos++;
+                    }
+                    if (num < 0)
+                    {
+                        negativos++;
+                    }
+                    if (num == 0)
+                    {
+                        cero++;
+                    }
+                }
+
+
+            } while (opcion != "n");
+            Console.WriteLine("numeros negativos: " + negativos);
+            Console.WriteLine("numeros positivos: " + positivos);
+            Console.WriteLine("numeros igual a cero: " + cero);
+        }
+    }
+}
