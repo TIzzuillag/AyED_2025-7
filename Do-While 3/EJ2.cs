0fff7d8644eeb0ae1f301de0afad82eab652a3e8
TIzzuillag <galluzzitizi@gmail.com>
2026-06-09 15:40:10 -0300

    Add files via upload

--- Do-While 3/EJ2.cs
+++ Do-While 3/EJ2.cs
@@ -4,51 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication4
+namespace ConsoleApplication5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
             string opcion;
-            int porcentaje=0;
-            int p = 0;
-            int a = 0;
+            double cantidad = 0;
+            double p = 0;
+            double a = 0;
             do
             {
-               Console.WriteLine("quiere ingresar un alumno(y/n)");
-opcion = (Console.ReadLine());
-               
+                Console.WriteLine("quiere ingresar un alumno(y/n)");
+                opcion = (Console.ReadLine());
+
                 if (opcion == "y")
                 {
-                  //  Console.Write("ingrese el nombre del alumno: ");
-                   // string nombre = (Console.ReadLine());
+                     Console.Write("ingrese el nombre del alumno: ");
+                     string nombre = (Console.ReadLine());
                     Console.Write("ingrese su estado: ");
                     string presente = (Console.ReadLine());
-                    if(presente=="p")
+                    if (presente == "p")
                     {
-                      
+
                         p++;
                     }
                     if (presente == "a")
                     {
                         a++;
-                       
+
                     }
-                porcentaje++;
+                    cantidad++;
+
                 }
-               
+
 
 
 
             } while (opcion != "n");
-            Console.WriteLine("hubieron "+p+" presente");
-            Console.WriteLine("hubieron " + a + " ausentes"      +porcentaje   );
+            Console.WriteLine("hubieron " + p + " presente");
+            Console.WriteLine("hubieron " + a + " ausentes" );
 
-            int presen = a / porcentaje;
-            int presen1 = presen * 100;
-            Console.WriteLine("porcentaje de presentismo: "+presen1+"%");
+            double presen = p / cantidad;
+            double porcentaje = presen * 100;
+            Console.WriteLine("porcentaje de presentismo: " + porcentaje + "%");
         }
     }
 }
