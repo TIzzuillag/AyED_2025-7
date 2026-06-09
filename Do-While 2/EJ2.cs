00de7aa50d1a788431ce875a4db1a998656ad82e
TIzzuillag <galluzzitizi@gmail.com>
2026-06-09 14:43:47 -0300

    Add files via upload

---
+++ Do-While 2/EJ2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int correcto = 42;
+            Console.WriteLine("ADIVINA EL NUMERO");
+            int num;
+            int intentos = 0;
+            do
+            {
+
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > correcto)
+                {
+                    Console.WriteLine("el numero es menor");
+                    intentos++;
+                }
+                if (num < correcto)
+                {
+                    Console.WriteLine("el numero es mayor");
+                    intentos++;
+                }
+            } while (num != correcto && intentos != 5);
+            if (intentos == 5)
+            {
+                Console.WriteLine("te excedite en intentos");
+            }
+            if (intentos < 5)
+            {
+                Console.WriteLine("CORRECTO");
+                Console.WriteLine("lo resolviste en " + intentos + " intentos");
+            }
+        }
+    }
+}
---
+++ Do-While 2/EJ4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+           
+            int num;
+            string opcion;
+            int contador = 0;
+            do
+            {
+                Console.WriteLine("quiere ingresar un numero(y/n)");
+                opcion = (Console.ReadLine());
+                if (opcion=="y")
+                {
+                    Console.Write("ingrese un numero: ");
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+               
+            } while (opcion != "n");
+            Console.WriteLine("hola mundo");
+        }
+    }
+}
