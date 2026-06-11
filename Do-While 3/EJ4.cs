ea8f593e97558088b62d53da7cbae6281f346516
TIzzuillag <galluzzitizi@gmail.com>
2026-06-11 14:17:16 -0300

    Add files via upload

---
+++ Do-While 3/EJ4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int minimo = 0;
+            int contador = 0;
+
+            do
+            {
+                Console.Write("Ingrese un número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+             
+                if (num >= 0)
+                {
+                    contador++;
+
+
+                    if (contador == 1)
+                    {
+                        minimo = num;
+                    }
+
+                     if (num < minimo)
+                    {
+                        minimo = num;
+                    }
+                }
+
+            } while (num >= 0);
+
+         
+            if (contador > 0)
+            {
+                Console.WriteLine("El número más chico es: " + minimo);
+            }
+          
+        }
+    }
+}
---
+++ Do-While 3/EJ5.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int num2;
+            int plata = 100;
+            int apuesta;
+         
+
+            do
+            {
+                Console.Write("Ingrese su apuesta: ");
+                apuesta = Convert.ToInt32(Console.ReadLine());
+               
+                if (apuesta<plata && apuesta!= 0)
+                {
+                 
+                 Console.Write("Ingrese el primer dado: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingrese el segundo dado: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                    int suma = num + num2;
+                    Console.WriteLine("valor del dado: "+suma);
+                   if (suma==7 || suma==11)
+                    {
+                        plata = plata + apuesta * 2;
+                        Console.WriteLine("GANASTE, tu dinero es de: " + plata);
+      
+                    }
+                    else
+                    {
+                        plata = plata - apuesta;
+                        Console.WriteLine(" PERDISTE, tu dinero es de: " + plata);
+                    }
+                }
+
+
+
+
+            } while (apuesta != 0 && plata > 0);
+
+            Console.WriteLine("te quedaste sin dinero");
+        }
+
+    }
+}
