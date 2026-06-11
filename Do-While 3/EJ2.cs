f048aa7b503d23262c0a47b06e19ffd8b3e7ab67
TIzzuillag <galluzzitizi@gmail.com>
2026-06-11 14:54:33 -0300

    Add files via upload

---
+++ Do-While 3/EJ2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio;
+            double subtotal = 0;
+            double descuento = 0;
+            double total;
+
+            Console.WriteLine("Ingrese los precios de los productos");
+
+            do
+            {
+                Console.Write("Precio: ");
+                precio = Convert.ToDouble(Console.ReadLine());
+
+                if (precio > 0)
+                {
+                    subtotal += precio;
+                }
+
+            } while (precio != 0);
+
+            if (subtotal > 15000)
+            {
+                descuento = subtotal * 0.10;
+            }
+
+            total = subtotal - descuento;
+
+
+            Console.WriteLine("Subtotal: " + subtotal);
+            Console.WriteLine("Descuento: " + descuento);
+            Console.WriteLine("Total a pagar: " + total);
+            Console.ReadKey();
+        }
+    }
+}
+    
+
