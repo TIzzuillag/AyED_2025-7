2036895d1b76115a8a88c9643ca6ca318db28403
TIzzuillag <galluzzitizi@gmail.com>
2026-08-11 16:39:33 -0300

    Add files via upload

---
+++ A y V/A y V 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] temperaturas = new double[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Ingrese la temperatura del día " + (i + 1));
+                temperaturas[i] = double.Parse(Console.ReadLine());
+            }
+
+         
+            double mayor = temperaturas[0];
+            double menor = temperaturas[0];
+            double suma = 0;
+
+            
+            for (int i = 0; i < 7; i++)
+            {
+                if (temperaturas[i] > mayor)
+                {
+                    mayor = temperaturas[i];
+                }
+
+                if (temperaturas[i] < menor)
+                {
+                    menor = temperaturas[i];
+                }
+
+                suma = suma + temperaturas[i];
+            }
+
+            double promedio = suma / 7;
+
+            
+            Console.WriteLine();
+            Console.WriteLine("Temperatura más alta: " + mayor);
+            Console.WriteLine("Temperatura más baja: " + menor);
+            Console.WriteLine("Promedio semanal: " + promedio);
+        }
+
+    }
+}
