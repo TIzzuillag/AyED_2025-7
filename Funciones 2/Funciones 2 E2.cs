867f5030328d77791db1fa8ad8cd552a22c66ef6
TIzzuillag <galluzzitizi@gmail.com>
2026-09-08 14:46:55 -0300

    Add files via upload

---
+++ Funciones 2/Funciones 2 E2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPerfecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perfecto();
+        }
+
+        static void Perfecto()
+        {
+            int num;
+            int suma = 0;
+
+            Console.Write("Ingrese un numero entero positivo: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    suma += i;
+                }
+            }
+
+            if (suma == num)
+            {
+                Console.WriteLine("El numero es perfecto.");
+            }
+            else
+            {
+                Console.WriteLine("El numero no es perfecto.");
+            }
+        }
+    }
+}
